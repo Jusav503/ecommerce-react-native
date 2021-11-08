6dcce54983d0f2b09324fbe34ad74a492bb22618
Justice Velasco <justicevgomez001@gmail.com>
2021-11-08 19:07:06 +0100

    AWS amplify implemented

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Amplify from "aws-amplify";
+import { withAuthenticator } from "aws-amplify-react-native";
 
-import Router from './src/router';
+import Router from "./src/router";
+import config from "./src/aws-exports";
+Amplify.configure(config);
 
-export default function App() {
+const App = () => {
   return (
     <SafeAreaProvider>
-      <StatusBar style="dark" backgroundColor= "cyan" />
+      <StatusBar style="dark" backgroundColor="cyan" />
       <Router />
     </SafeAreaProvider>
   );
-}
+};
+
+export default withAuthenticator(App);
